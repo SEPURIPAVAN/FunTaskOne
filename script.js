8bac317b37a3b759bff59969c454737f3c1b29b9
S Pavan <sepuripavan07@gmail.com>
2025-06-04 22:46:17 +0530

    some changes

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
 let mainno=Math.floor(Math.random()*100)+1;
 
 let guessno=document.getElementById("numip");
-
 let p=document.querySelector("p")
-
 let form=document.querySelector("form")
 
+let clickcount=0
 
 let div=document.querySelector("div")
 
-
 form.addEventListener("submit",function(e){
     e.preventDefault();
-        
+    
+    clickcount+=1;
     let guess=guessno.value;
     guessno.value="";
     div.innerHTML="";
     let p1=document.createElement("p")
-    let ul=document.createElement("p")
+    let p2=document.createElement("p")
+    let p3=document.createElement("p")
+    let p4=document.createElement("p")
     p1.textContent="Feedback:"
     if(guess==mainno){
-        ul.textContent="You Win"
+        p2.textContent="You Win"
     }else if(guess>mainno){
-        ul.textContent="Your Number is Larger"
+        p2.textContent="Your Number is Larger"
     }else{
-        ul.textContent="Your Number is Smaller"
+        p2.textContent="Your Number is Smaller"
     }
+    p3.textContent="No of attemps:";
+    p4.textContent=clickcount;
 
-
-
-    div.append(p1,ul);
+    div.append(p1,p2,p3,p4);
 })
