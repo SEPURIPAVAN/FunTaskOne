401f632fcf44f9ec174341f592c4b5d4ed84d915
S Pavan <sepuripavan07@gmail.com>
2025-06-04 22:22:03 +0530

    single user done

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 let mainno=Math.floor(Math.random()*100)+1;
 
 let guessno=document.getElementById("numip");
+
 let p=document.querySelector("p")
 
 let form=document.querySelector("form")
 
-let body=document.querySelector("body")
+
 let div=document.querySelector("div")
 
 
 form.addEventListener("submit",function(e){
     e.preventDefault();
-
+        
     let guess=guessno.value;
-
+    guessno.value="";
+    div.innerHTML="";
     let p1=document.createElement("p")
-    let ul=document.createElement("ul")
-    let li1=document.createElement("li")
-    let li2=document.createElement("li")
-    
-    p.innerHTML=`<p>FeedBack:</p>`
-    li1.textContent=guess;
-    li2.textContent=mainno;
-    div.append(ul,li1,li2);
+    let ul=document.createElement("p")
+    p1.textContent="Feedback:"
+    if(guess==mainno){
+        ul.textContent="You Win"
+    }else if(guess>mainno){
+        ul.textContent="Your Number is Larger"
+    }else{
+        ul.textContent="Your Number is Smaller"
+    }
+
+
+
+    div.append(p1,ul);
 })
